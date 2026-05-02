781133829ce5ebf7e6922be7b5cdde8c9543b5b4
Jia Le Cheong <jialecheong@Jia-Les-MacBook-Air.local>
2026-05-03 01:37:40 +0800

    feat(dev): add in-app DevModeService for Online/Offline/Emulator testing

---
+++ lib/utils/dev_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+
+enum DevMode { online, offline, emulator }
+
+class DevModeService {
+  static final ValueNotifier<DevMode> current = ValueNotifier<DevMode>(DevMode.online);
+  static void set(DevMode mode) {
+    current.value = mode;
+  }
+}
